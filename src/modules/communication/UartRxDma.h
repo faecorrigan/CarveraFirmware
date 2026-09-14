f355/Carvera_Community_Firmware

Add UART receive DMA support

src/modules/communication/UartRxDma.h
@@ -1,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+namespace uart_rx_dma {
+
+void initialize();
+bool try_get(uint8_t& byte);
+bool take_error();
+
+}  // namespace uart_rx_dma

faecorrigan/CarveraFirmware

src/modules/communication/UartRxDma.h
@@ -1,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+namespace uart2_rx_dma {
+
+void initialize();
+bool try_get(uint8_t& byte);
+bool take_error();
+
+}  // namespace uart2_rx_dma